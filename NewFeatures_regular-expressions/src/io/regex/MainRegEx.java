728919b1d7e8992f5fc29cab5aa822da4bf88710
atafs <atafs@iscte-iul.pt>
2016-01-17 18:37:31 +0000

    FEATURE: regex functionalities to search and replace strings

--- NewFeatures_regular-expressions/src/io/regex/MainRegEx.java
+++ NewFeatures_regular-expressions/src/io/regex/MainRegEx.java
@@ -9,7 +9,7 @@ public class MainRegEx {
 	public static void main(String[] args) {
 		
 		//DATA TO ANALYSE ###############################
-		String longString = "Americo Tomas  CA  AK 12345 PA (412)555-1212 johnsmith";
+		String longString = "Americo Tomas  CA  AK 12345 PA (412)555-1212 atafs@iscte-iul.com";
 		String strangeString = " 1Z aaa **** *** {{{ {{ { ";
 		
 		//REGEX search
@@ -29,10 +29,14 @@ public class MainRegEx {
 		 * +			= what ever proceeds, must happens one or more times
 		 * . ^ * + ? 	= need to backslash (protected character) 
 		 * {} [] \ | ()	= need to backslash (protected character) 
-		 *
+		 * \\w			= type caracter
+		 * \\W			= NOT any type carater
+		 * *			= happen 0 or more times
+		 * ?			= could exit or not (uncertain)
 		 * */
 		
-		//SEARCH ########################################
+		//BASIC SEARCH ########################################
+		
 		//word that is 2 to 20 characters in length
 		//[A-Za-z]{2,20} \\w{2,20}
 		regexChecker("\\s[A-Za-z]{2,20}\\s", longString);
@@ -43,13 +47,50 @@ public class MainRegEx {
 		
 		//States that start with AC or AD
 		//A[KLRZ]|C[AOT]
-		System.out.println("\n--------SEARCH---------");
+		System.out.println("\n--------SEARCH A[KLRZ] | C[AOT] ---------");
 		regexChecker("A[KLRZ] | C[AOT]", longString);
 		
 		//Protected character { (1 or more)
-		System.out.println("\n--------SEARCH---------");
+		System.out.println("\n--------SEARCH {1,}---------");
 		regexChecker("(\\{{1,})", strangeString);
-
+		
+		//Protected character { (1 or more)
+		System.out.println("\n--------SEARCH + ---------");
+		regexChecker("(\\{+)", strangeString);
+		
+		//Anything . (3 or more)
+		System.out.println("\n--------SEARCH .{3} ---------");
+		regexChecker(".{3}", strangeString);
+		
+		//word type caracter \\w
+		System.out.println("\n--------SEARCH .{3} ---------");
+		regexChecker(".{3}", strangeString);
+		
+		//ADVANCED SEARCH ########################################
+		
+		//email address: xxx@yyy.zzzz
+		//can have: [A-Za-z0-9.%-]
+		//have the symbol: @
+		System.out.println("\n--------SEARCH email ---------");
+		regexChecker("[A-Za-z0-9.%-]+@[A-Za-z0-9.%-]+.[A-Za-z]{2,4}", longString);
+		
+		//phone number
+		//(xxx)yyy-yyyy
+		//combinations possible: 1-(412)555-1212 (412)555-1212 412-555-1234 412 555-1234
+		System.out.println("\n--------SEARCH phone ---------");
+		regexChecker("([0-9]( |-)?)?(\\(?[0-9]{3}\\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})", longString);
+		
+		//BASIC REPLACE ########################################
+		
+		System.out.println("\n--------REPLACE VOID space by comma ---------");
+		regexReplace(longString);
+		System.out.println("Initial Data: " + longString);
+		
+		System.out.println("\n--------REPLACE RETURN space by comma ---------");
+		String s = regexReplaceReturnString(longString);
+		System.out.println();
+		System.out.println("Function Method: " + s);
+		System.out.println("Initial Data: " + longString);
 	}
 	
 	/** STATIC PROCEDURE: allow the user to search for a specific string of values */
@@ -72,5 +113,30 @@ public class MainRegEx {
 			System.out.println("Start Index = " + regexMatcher.start());
 			System.out.println("End Index = " + regexMatcher.end());
 		}
+	}
+	
+	/** STATIC PROCEDURE: replace all spaces (\\s) with comma and spaces (, ) */
+	public static void regexReplace(String str2replace) {
+		//API PATTERN class
+		Pattern replace = Pattern.compile("\\s+");
+		
+		//API MATCHER class
+		Matcher regexMatcher = replace.matcher(str2replace.trim());
+		
+		//PRINT
+		System.out.println();
+		System.out.println("Void Method: " + regexMatcher.replaceAll(", "));
+	}
+	
+	/** STATIC FUNCTION: replace all spaces (\\s) with comma and spaces (, ) and returns a String */
+	public static String regexReplaceReturnString(String str2replace) {
+		//API PATTERN class
+		Pattern replace = Pattern.compile("\\s+");
+		
+		//API MATCHER class
+		Matcher regexMatcher = replace.matcher(str2replace.trim());
+		
+		//RETURN
+		return regexMatcher.replaceAll(", ");
 	}
 }
